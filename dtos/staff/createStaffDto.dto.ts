960798b086a4ff5635d3a2721e54d1be19c4b780
danssinia <danmerethio19@gmail.com>
2025-12-21 20:06:19 +0300

    staff DTO and controller modified

--- dtos/staff/createStaffDto.dto.ts
+++ dtos/staff/createStaffDto.dto.ts
@@ -5,19 +5,64 @@ import {
     IsEnum,
     IsUUID,
     MinLength,
-    IsNumber
+    IsNumber,
+    IsEmail
 } from "class-validator";
 
 
 
 export class CreateStaffDto {
+    //the user_id might be commented (i will check it)
+    @IsUUID()
+    @IsString()
+    user_id!:string
+
+    @IsString()
+    @MinLength(3)
+    first_name!:string
+
+    @IsString()
+    @MinLength(3)
+    last_name!:string
+
+    @IsEmail()
+    @IsString()
+    email!:string
+
+    @IsString()
+    username!:string
+
+    @IsString()
+    @MinLength(8)
+    password!:string
+
+    @IsString()
+    @MinLength(10)
+    @IsOptional()
+    phone?:string
+
+    @IsDateString()
+    @IsOptional()
+    date_of_birth?:Date 
+
+    @IsString()
+    @IsOptional()
+    address?:string
+
+    @IsUUID()
+    @IsString()
+    role_id!:string
+
+
+
+    //here below the need to be checked from the staff table
     @IsUUID()
     @IsString()
     staff_id!:string
 
-    @IsUUID()
-    @IsString()
-    user_id!:string
+    // @IsUUID()
+    // @IsString()
+    // user_id!:string
 
     @IsDateString()
     hire_date!:string
