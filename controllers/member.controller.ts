5c8181504d7acd0c6958e7dc07d4ae128aba41dd
danssinia <danmerethio19@gmail.com>
2025-12-31 09:43:29 +0300

    member controller modified

--- controllers/member.controller.ts
+++ controllers/member.controller.ts
@@ -1,21 +1,37 @@
-import { Request , Response } from "express";
+import { Request, Response } from "express";
 import { MemberService } from "../services/member.service";
 import { CreateMemberDto } from "../dtos/member/createMemberDto.dto";
 import { UpdateMemberDto } from "../dtos/member/updateMemberDto.dto";
 import { plainToClass } from "class-transformer";
 export class MemberController {
-    private memberService:MemberService
-    constructor () {
-        this.memberService =  new MemberService()
+    private memberService: MemberService
+    constructor() {
+        this.memberService = new MemberService()
     }
-    createMember = async (req:Request,res:Response) => {
-      
+    createMember = async (req: Request, res: Response) => {
+
         //calling the service
-        const data= await this.memberService.createMember()
+        const data = await this.memberService.createMember()
         return res.status(200).json({
-            success:true,
-            message:data.message
+            success: true,
+            message: data.message
         })
-      
+
+    }
+
+    getAllMemebers = async (req: Request, res: Response) => {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    getMemberById = async (req: Request, res: Response) => {
+        try {
+
+        } catch (error) {
+
+        }
     }
 }
